bef49f32f843f8aba9576fb41432ec7747e86984
Fabio Nettis <nettisfabio@gmail.com>
2024-04-05 10:29:46 +0200

    fix: add missing dts file to build output

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,10 @@
+import dts from "bun-plugin-dts";
+
 await Bun.build({
   minify: true,
   target: "node",
   outdir: "./build",
+  plugins: [dts()],
   entrypoints: ["./index.ts"],
   naming: { entry: "[name].mjs" },
 });
-
-export {};
